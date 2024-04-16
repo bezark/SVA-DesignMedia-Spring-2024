48af31ea58e341bc7eb1ad2d4ce347af4bbcae5b
bezark <jdb459@nyu.edu>
2024-04-16 13:41:56 -0400

    WEbXR AR

--- Resources/ExportTemplates/WEBXR Demo/Builds/Web/index.service.worker.js
+++ Resources/ExportTemplates/WEBXR Demo/Builds/Web/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1712700636|40152854";
+const CACHE_VERSION = "1713302963|1977054498";
 const CACHE_PREFIX = "WEBXR Demo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
